9143fd8940fbf903f768b4da3ea005ff0bcc5ad7
Yuragranovsky <yuragranovsky@gmail.com>
2021-10-27 21:06:36 +0300

    adds new methods of CW #7 and default stubs

---
+++ src/telran/util/RemoveAllPredicate.java
@@ -0,0 +1,17 @@
+package telran.util;
+
+import java.util.function.Predicate;
+
+public class RemoveAllPredicate<T> implements Predicate<T> {
+ List<T> others;
+ 
+	public RemoveAllPredicate(List<T> others) {
+	this.others = others;
+}
+
+	@Override
+	public boolean test(T t) {
+		return others.contains(t);
+	}
+
+}
